ad0367599c034faf851e07e14a5740764e570333
Elinilson Vital <vital@EVitalMBAir.local>
2023-10-22 23:46:18 -0300

    Changes to run from the web folder ./run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+
 virtualenv env
 source env/bin/activate
-pip3 install -r requirements.txt
-pip3 install -r requirements_ai.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-
-#xdg-open http://127.0.0.1:8000/
+python manage.py collectstatic -y
+# http://127.0.0.1:8000/
 
 python manage.py runserver 
