1988a6e7e2af61de445dc2367d2f624a7fedae43
gustavo_honda <gustavo_honda@usp.br>
2023-11-09 16:42:36 -0300

    fix: css/newstyle.css working

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic -y
+python manage.py collectstatic 
 # http://127.0.0.1:8000/
 
 python manage.py runserver 
