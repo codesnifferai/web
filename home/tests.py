05421e559bc4ace99e16408be07098d729f1896b
Elinilson Vital <vital@EVitalMBAir.local>
2023-11-21 19:22:51 -0300

    Added testing methods

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
-
+import os
+from .models import CodeSnippet, Scores
+from IACode.sniffer import Sniffer
 # Create your tests here.
+
+class SimpleTest(TestCase):
+
+    def test_index_page(self):
+        print("***=== Index page get test")
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_api(self):
+        print("***=== Api Rest get test")
+        response = self.client.get('/api/v1/codesnifferai/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_code(self):
+        print("***=== Code page post test")
+        directory = os.getcwd()
+        # print(directory)
+        java_file = open(directory + "/class2test/org.apache.oozie.command.XCommand.java", "r")
+
+        # read whole file to a string
+        data = java_file.read()
+
+        # close file
+        java_file.close()
+
+        response = self.client.post('/code/', {'code': data})
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_ia_model(self):
+        print("***=== Testing the AI model for class org.apache.oozie.command.XCommand.java (20k)")
+        result_assets = {'Data Class': 0.07, 'God Class': 0.02, 'Model Class': 1.0, 'Schizofrenic Class': 0.05}
+        directory = os.getcwd()
+        java_file = open(directory + "/class2test/org.apache.oozie.command.XCommand.java", "r")
+        code = java_file.read()
+        java_file.close()
+
+        cs = CodeSnippet()
+        cs.code = code
+        cs.source = "web"
+        cs.save()
+
+        try:
+            code_analysis_result = Sniffer.CodeAnalysis(code)
+            for d in code_analysis_result:
+                print(d)
+                scores = Scores()
+                scores.name = d
+                scores.code = cs
+                scores.value = code_analysis_result[d]
+                scores.save()
+
+            result = Sniffer.CodeAnalysis(code)
+            result_model = {key: result[key] for key in result if result[key] > 0}
+            for key  in result_model:
+                print(" Model value: {} , expected value: {}".format(result_model[key], result_assets[key]))
+                self.failUnlessEqual(result_model[key], result_assets[key])
+
+        except Exception as error:
+            raise error
--- home/views.py
+++ home/views.py
@@ -9,11 +9,12 @@ from IACode.sniffer import Sniffer
 from home.forms import HomeForm
 from .models import CodeSnippet, Scores
 import pandas as pd
+from chartkick.django import BarChart, PieChart
 
 @csrf_exempt
 def code(request):
 
-    code_analysis_result = None
+    unit_chart = "No codes to check. Send the code in the text box."
     code = None
     template = loader.get_template("home/code.html")
 
@@ -25,19 +26,31 @@ def code(request):
         cs.source = "web"
         cs.save()
 
-        code_analysis_result = Sniffer.CodeAnalysis(code)
-        for d in code_analysis_result:
-            print(d)
-            scores = Scores()
-            scores.name = d
-            scores.code = cs
-            scores.value = code_analysis_result[d]
-            scores.save()
+        try:
+            code_analysis_result = Sniffer.CodeAnalysis(code)
+            for d in code_analysis_result:
+                print(d)
+                scores = Scores()
+                scores.name = d
+                scores.code = cs
+                scores.value = code_analysis_result[d]
+                scores.save()
 
-        code_analysis_result = Sniffer.CodeAnalysis(code)
+            result = Sniffer.CodeAnalysis(code)
+            result_gtz = {key: result[key] for key in result if result[key] > 0}
+            unit_chart = PieChart(result_gtz, donut=True, percent=True)
+        except Exception as error:
+            unit_chart = error
 
+    scores = Scores.objects.values('name').order_by('name').annotate(value=Avg('value')*100)
+    cumulative_dic = {}
+    for object in scores:
+        cumulative_dic[object["name"]] = object["value"]
+    # df = pd.DataFrame(scores)
+    cumulative_chart = BarChart(cumulative_dic, colors= [["#DFFF00", "#FFBF00", "#FF7F50", "#DE3163", "#9FE2BF", "#40E0D0", "#6495ED", "#CCCCFF"]])
     context = {
-        "code_analysis_result": code_analysis_result,
+        "cumulative_chart": cumulative_chart,
+        "unit_chart": unit_chart,
         "form": HomeForm(),
         "code": code,
     }
